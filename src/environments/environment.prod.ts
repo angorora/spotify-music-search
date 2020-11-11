46ca7c064e3b5b72c915acdebdcfa9ce2833c753
Admin <allen.ngorora@gmail.com>
2020-11-11 17:50:19 +0200

    removed keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
   production: true,
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/',
-  clientId: '691c1e40e4e8425c81a9c989c7f5f56a',
-  clientSecret: '35b2af8794054825965d3c46f4233be8',
 };
